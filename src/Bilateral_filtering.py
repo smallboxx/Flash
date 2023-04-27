a334a6fdab0964de7439a895fde8ab45e44ad73b
maoyumeng <17360286881@163.com>
2023-04-27 13:30:59 +0800

    Update Bilateral_filtering.py

--- src/Bilateral_filtering.py
+++ src/Bilateral_filtering.py
@@ -13,47 +13,39 @@ def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, lengt
     if iteration == total:
         sys.stdout.write('\n')
 
-# 定义一个辅助函数，用于计算像素之间的空间距离
-def distance(x1, y1, x2, y2):
-    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-
-# 定义一个辅助函数，用于计算像素之间的灰度值相似度
-def similarity(pixel1, pixel2, sigmaColor):
-    diff = np.subtract(pixel1, pixel2, dtype=np.float64)
-    return np.exp(-(np.sum(diff**2)) / (2 * sigmaColor**2))
 
 def bilateral_filter(L, diameter, sigma_color, sigma_space):
-        # 获取输入图像的高度和宽度
-    height, width = L.shape[:2]
+    output = np.zeros_like(L)
+    radius = diameter // 2
+    L= np.divide(L,255)
+    for i in range(radius,L.shape[0]-radius):
+        for j in range(radius,L.shape[1]-radius):
+            pixel_value = 0.0
+            weight_sum = 0.0
+            for k in range(i - radius, i + radius + 1):
+                for l in range(j - radius, j + radius + 1):
+                    # 计算空间距离
+                    spatial_distance = pow((i - k),2) + pow((j - l),2)
 
-    # 创建一个空白的输出图像
-    img_filtered = np.zeros_like(L)
+                    # 计算颜色距离
+                    color_distance =pow((L[i, j] - L[k, l]),2)
 
-    # 对于每个像素，计算权重并进行加权平均
-    for i in range(height):
-        for j in range(width):
-            # 计算当前像素的权重
-            pixel_weight = np.float64(0)
-            pixel_sum = np.zeros(3, dtype=np.float64)
-            for k in range(height):
-                for l in range(width):
-                    # 计算空间距离和灰度值相似度
-                    d = distance(i, j, k, l)
-                    if d <= diameter:
-                        s = similarity(L[i, j], L[k, l], sigma_color)
-                        # 计算像素的权重
-                        w = s * np.exp(-(d**2) / (2 * sigma_space**2))
-                        # 计算加权平均
-                        pixel_sum += w * L[k, l]
-                        pixel_weight += w
-            # 将加权平均结果保存到输出图像中
-            img_filtered[i, j] = int(pixel_sum / pixel_weight)
-            print([i,j],[height,width])
-    return img_filtered
+                    # 计算权重
+                    weight = np.exp(-spatial_distance/ (2 * sigma_space ** 2))
+                    weight *= np.exp(-color_distance / (2 * sigma_color ** 2))
+
+                    # 累加像素值和权重
+                    pixel_value += weight * L[k, l]
+                    weight_sum += weight
+            output[i, j] = pixel_value / weight_sum
+            print_progress_bar((i-25)*(L.shape[1]-diameter)+(j-25)+1,(L.shape[1]-diameter)*(L.shape[0]-diameter),"start","complete")
+    output=np.uint8(np.multiply(output,255))
+    return output
 
 if __name__=="__main__":
     diameter = 50
     input = cv2.imread("data\\lamp\\lamp_ambient.tif",cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
+    input=cv2.resize(input,[200,200])
     input = cv2.cvtColor(input,cv2.COLOR_LBGR2Lab)
     L=input[:,:,0]
     a=input[:,:,1]
